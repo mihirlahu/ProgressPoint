f507972eda47666a5389c97471640d685a65569c
Vivek Joshi <vivekjoshi@Viveks-MacBook-Pro.local>
2023-03-19 20:06:13 -0400

    added route for developers

--- src/controllers/developer.controller.js
+++ src/controllers/developer.controller.js
@@ -26,3 +26,38 @@ const Login = async function (req, res) {
     res.status(400).send(e);
   }
 };
+
+const showHomePage = async function(req, res) {
+  try {
+      const user = req.user
+      res.render('UserHome', {
+          user
+      })
+
+  } catch (e) {
+
+      res.status(500).send();
+  }
+}
+
+const loadHome = async function(req, res) {
+  var following = Object.keys(req.user.following)
+
+  const documents = await Document.find({ $or: [{ $and: [{ classRoomId: { $in: following } }, { teacherId: { $exists: true } }] }, { $and: [{ classRoomId: { $in: following } }, { studentId: { $nin: req.user._id } }] }] }).populate('teacherId', 'name email age').populate('studentId', 'name email age')
+
+  documents.forEach(element => {
+      if (element.teacherId) {
+          element.populate('teacherId', 'name email age').execPopulate()
+      } else {
+          element.populate('studentId', 'name email age').execPopulate()
+      }
+  });
+  res.send(documents)
+}
+
+module.exports = {
+  Register: Register,
+  Login: Login,
+  showHomePage,
+  loadHome
+}
