759927d143de62476a11d773ad545ce547d57269
sushma <sushmadeegoju98@gmail.com>
2023-04-07 16:07:04 -0400

    Added developer routes

--- src/routers/developer.js
+++ src/routers/developer.js
@@ -18,12 +18,21 @@ router.get(
   auth,
   developerControllers.getDocument
 );
+
+router.post("/register/developer", developerControllers.Register);
+router.post("/login/developer", developerControllers.Login);
+router.get("/developer/home", auth, developerControllers.showHomePage)
+router.get("/developer/loadHome", auth, developerControllers.loadHome)
+router.post('/developer/meetingRoom/uploadDocument', auth, developerControllers.Upload.single('fileUpload'), developerControllers.UploadDocument)
+router.get('/developer/meetingRoom/getDocument', auth, developerControllers.getDocument)
+router.post("/search", auth, developerControllers.searchMeetingRoom)
 router.get("/loadSearch", auth, developerControllers.loadSearch);
 router.get("/developer/loadSearch", auth, developerControllers.loadTeamDetails);
-router.post("/developer/loadSearch", auth, developerControllers.loadTeamRoom);
-router.post("/search", auth, developerControllers.searchClassRoom);
+router.post("/developer/loadSearch", auth, developerControllers.loadMeetingRoom);
 router.get("/developer/profile", auth, developerControllers.Profile);
 router.get("/developer/logout", auth, developerControllers.Logout);
 router.patch('/developer/profile/update', auth, developerControllers.UpdateProfile);
+router.post('/developer/follow', auth, developerControllers.developerRoomFollow);
+
 
 module.exports = router;
